be93c5924afacce56e1fc3ecbe1cfab0d3265116
sigltech <101203107+sigltech@users.noreply.github.com>
2022-06-18 00:02:09 +0100

    fixed about me postioning and some seciton 1 styling

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,2 @@
-// let frameworks = document.querySelector('#frameworks');
-
-// function renderFrameworks() {
-//   let liElement = document.createElement('li');
-//   let img = document.createElement('img');
-//   liElement.classList.add('framework-icons');
-
-//   for(let i = 0; i < 8; i++) {
-//     counter = 1
-//     img.src.add(`../../assets/imgs/frameworks-logos/${counter}`)
-//     frameworks.innerHTML = `
-    
-//     `
-//     counter + 1
-//   }
-// }
-
-// renderFrameworks()
 
 
