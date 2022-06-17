8ca8544274d41bcf895ae3c1762b7dc4fc4e619d
sigltech <101203107+sigltech@users.noreply.github.com>
2022-06-17 21:45:06 +0100

    added socials tab and frameworks to section 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,20 @@
-$(window).scroll(function() {
-    $('header').css({
-      opacity: function() {
-        var opacity = 0;
-        //TODO:
-        //Set opacity to a higer value whilst user scrolls
-        return opacity;
-      }
-    });
-  });
+// let frameworks = document.querySelector('#frameworks');
+
+// function renderFrameworks() {
+//   let liElement = document.createElement('li');
+//   let img = document.createElement('img');
+//   liElement.classList.add('framework-icons');
+
+//   for(let i = 0; i < 8; i++) {
+//     counter = 1
+//     img.src.add(`../../assets/imgs/frameworks-logos/${counter}`)
+//     frameworks.innerHTML = `
+    
+//     `
+//     counter + 1
+//   }
+// }
+
+// renderFrameworks()
+
+
