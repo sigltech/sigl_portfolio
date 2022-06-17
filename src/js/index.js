78077fa702d617dfe696d790f75970561fc779a3
sigltech <101203107+sigltech@users.noreply.github.com>
2022-06-17 18:59:36 +0100

    updates to responsivness

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+$(window).scroll(function() {
+    $('header').css({
+      opacity: function() {
+        var opacity = 0;
+        //TODO:
+        //Set opacity to a higer value whilst user scrolls
+        return opacity;
+      }
+    });
+  });
