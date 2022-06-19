f0dbd8c41be7e7221367680484ec8f3961a8f472
sigltech <101203107+sigltech@users.noreply.github.com>
2022-06-19 12:04:14 +0100

    scroll to top button added

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,20 @@
+let toTopbtn = document.querySelector('#bttBtn');
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+toTopbtn.addEventListener('click', topFunction)
 
 
